3a3c6def9e1bc6ad6e5151fc447cef3ac9659430
George-Okumu <gokumu12@gmail.com>
2023-08-18 17:25:06 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    n1 = 0
+    n2 = 1
+    fib = [n1]
+    # if (not length or length == 1):
+    #     print([0])
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    else:
+        for i in range(length - 1):
+            fib.append(n2)
+            nth = n1 + n2
+            n1 = n2
+            n2 = nth
+
+        print(fib)
+
+
+print_fibonacci(1)
